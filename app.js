20567a1c196fe6e0b61c74c9c42445f82fc6206f
Nermin Sehic <nsehic@rejectshop.com.au>
2020-12-16 17:17:35 +1100

    Enable command line tools

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ function formatTime(d) {
   return moment(number.toString(), "LT").format("HH:mm");
 }
 
+if (process.argv[3] === undefined) {
+  console.log("You must pass a filename for processing");
+  process.exit();
+}
+
 const columns = {
   week: 5,
   store: 4,
@@ -53,72 +58,82 @@ const columns = {
   },
 };
 
-xlsxFile("./files/ChristmasTradingHours.xlsx").then(async (rows) => {
-  let count = 0;
-  for (let i = 2; i < rows.length; i++) {
-    await sleep(TIMER);
-    if (rows[i][columns.week] === CURRENT_WEEK) {
-      const omneo_id = rows[i][columns.omneo_id];
-      const location = rows[i][columns.store];
-      count += 1;
-      const body = {
-        normal_hours: [
-          {
-            day_of_week: "MON",
-            open_at: formatTime(rows[i][columns.weekday.monday.open]),
-            close_at: formatTime(rows[i][columns.weekday.monday.close]),
-          },
-          {
-            day_of_week: "TUE",
-            open_at: formatTime(rows[i][columns.weekday.tuesday.open]),
-            close_at: formatTime(rows[i][columns.weekday.tuesday.close]),
-          },
-          {
-            day_of_week: "WED",
-            open_at: formatTime(rows[i][columns.weekday.wednesday.open]),
-            close_at: formatTime(rows[i][columns.weekday.wednesday.close]),
-          },
-          {
-            day_of_week: "THU",
-            open_at: formatTime(rows[i][columns.weekday.thursday.open]),
-            close_at: formatTime(rows[i][columns.weekday.thursday.close]),
-          },
-          {
-            day_of_week: "FRI",
-            open_at: formatTime(rows[i][columns.weekday.friday.open]),
-            close_at: formatTime(rows[i][columns.weekday.friday.close]),
-          },
-          {
-            day_of_week: "SAT",
-            open_at: formatTime(rows[i][columns.weekday.saturday.open]),
-            close_at: formatTime(rows[i][columns.weekday.saturday.close]),
-          },
-          {
-            day_of_week: "SUN",
-            open_at: formatTime(rows[i][columns.weekday.monday.open]),
-            close_at: formatTime(rows[i][columns.weekday.sunday.close]),
-          },
-        ],
-      };
+if (process.argv[2] === "website") {
+  updateWebsite(process.argv[3]);
+} else if (process.argv[2] === "google") {
+  console.log("This script is not supported yet");
+} else {
+  console.log("Unknown command");
+}
 
-      fetch(
-        `https://api.therejectshop.omneoapp.com/api/v3/locations/${omneo_id}`,
-        {
-          method: "PUT",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
-          },
-        }
-      ).then((res) => {
-        if (res.ok) {
-          console.log(location, res.status, `(${omneo_id})`);
-        } else {
-          console.log(location, body);
-        }
-      });
+function updateWebsite(filename) {
+  xlsxFile(filename).then(async (rows) => {
+    let count = 0;
+    for (let i = 2; i < rows.length; i++) {
+      await sleep(TIMER);
+      if (rows[i][columns.week] === CURRENT_WEEK) {
+        const omneo_id = rows[i][columns.omneo_id];
+        const location = rows[i][columns.store];
+        count += 1;
+        const body = {
+          normal_hours: [
+            {
+              day_of_week: "MON",
+              open_at: formatTime(rows[i][columns.weekday.monday.open]),
+              close_at: formatTime(rows[i][columns.weekday.monday.close]),
+            },
+            {
+              day_of_week: "TUE",
+              open_at: formatTime(rows[i][columns.weekday.tuesday.open]),
+              close_at: formatTime(rows[i][columns.weekday.tuesday.close]),
+            },
+            {
+              day_of_week: "WED",
+              open_at: formatTime(rows[i][columns.weekday.wednesday.open]),
+              close_at: formatTime(rows[i][columns.weekday.wednesday.close]),
+            },
+            {
+              day_of_week: "THU",
+              open_at: formatTime(rows[i][columns.weekday.thursday.open]),
+              close_at: formatTime(rows[i][columns.weekday.thursday.close]),
+            },
+            {
+              day_of_week: "FRI",
+              open_at: formatTime(rows[i][columns.weekday.friday.open]),
+              close_at: formatTime(rows[i][columns.weekday.friday.close]),
+            },
+            {
+              day_of_week: "SAT",
+              open_at: formatTime(rows[i][columns.weekday.saturday.open]),
+              close_at: formatTime(rows[i][columns.weekday.saturday.close]),
+            },
+            {
+              day_of_week: "SUN",
+              open_at: formatTime(rows[i][columns.weekday.monday.open]),
+              close_at: formatTime(rows[i][columns.weekday.sunday.close]),
+            },
+          ],
+        };
+
+        fetch(
+          `https://api.therejectshop.omneoapp.com/api/v3/locations/${omneo_id}`,
+          {
+            method: "PUT",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+            },
+          }
+        ).then((res) => {
+          if (res.ok) {
+            console.log(location, res.status, `(${omneo_id})`);
+          } else {
+            console.log(location, body);
+          }
+        });
+      }
     }
-  }
-});
+  });
+}
